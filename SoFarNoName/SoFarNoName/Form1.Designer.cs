621f9b3d48dfdba505d090de2aef0110a48c07ca
Pqoeodj <trevornggo@gmail.com>
2026-02-21 12:32:07 +0000

    a more recent update. Now ive learnt how to enter keybind inputs

--- SoFarNoName/SoFarNoName/Form1.Designer.cs
+++ SoFarNoName/SoFarNoName/Form1.Designer.cs
@@ -30,87 +30,54 @@ namespace SoFarNoName
         /// </summary>
         private void InitializeComponent()
         {
-            this.DisplayLbl = new System.Windows.Forms.Label();
-            this.DecrementBtn = new System.Windows.Forms.Button();
-            this.IncrementBtn = new System.Windows.Forms.Button();
-            this.SetBtn = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.TxtValue = new System.Windows.Forms.TextBox();
-            this.panel1.SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.WButton = new System.Windows.Forms.Timer(this.components);
+            this.SButton = new System.Windows.Forms.Timer(this.components);
+            this.DButton = new System.Windows.Forms.Timer(this.components);
+            this.AButton = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // DisplayLbl
+            // lblPlayer
             // 
-            this.DisplayLbl.AutoSize = true;
-            this.DisplayLbl.Location = new System.Drawing.Point(372, 180);
-            this.DisplayLbl.Name = "DisplayLbl";
-            this.DisplayLbl.Size = new System.Drawing.Size(35, 13);
-            this.DisplayLbl.TabIndex = 0;
-            this.DisplayLbl.Text = "label1";
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer.Location = new System.Drawing.Point(385, 216);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(91, 31);
+            this.lblPlayer.TabIndex = 0;
+            this.lblPlayer.Text = "Player";
             // 
-            // DecrementBtn
+            // WButton
             // 
-            this.DecrementBtn.Location = new System.Drawing.Point(129, 119);
-            this.DecrementBtn.Name = "DecrementBtn";
-            this.DecrementBtn.Size = new System.Drawing.Size(75, 23);
-            this.DecrementBtn.TabIndex = 1;
-            this.DecrementBtn.Text = "-";
-            this.DecrementBtn.UseVisualStyleBackColor = true;
-            this.DecrementBtn.Click += new System.EventHandler(this.ButtonClicked);
+            this.WButton.Interval = 1;
+            this.WButton.Tick += new System.EventHandler(this.W_Tick);
             // 
-            // IncrementBtn
+            // SButton
             // 
-            this.IncrementBtn.Location = new System.Drawing.Point(23, 119);
-            this.IncrementBtn.Name = "IncrementBtn";
-            this.IncrementBtn.Size = new System.Drawing.Size(75, 23);
-            this.IncrementBtn.TabIndex = 2;
-            this.IncrementBtn.Text = "+";
-            this.IncrementBtn.UseVisualStyleBackColor = true;
-            this.IncrementBtn.Click += new System.EventHandler(this.ButtonClicked);
+            this.SButton.Interval = 1;
+            this.SButton.Tick += new System.EventHandler(this.S_Tick);
             // 
-            // SetBtn
+            // DButton
             // 
-            this.SetBtn.Location = new System.Drawing.Point(23, 26);
-            this.SetBtn.Name = "SetBtn";
-            this.SetBtn.Size = new System.Drawing.Size(75, 23);
-            this.SetBtn.TabIndex = 3;
-            this.SetBtn.Text = "Set value ";
-            this.SetBtn.UseVisualStyleBackColor = true;
-            this.SetBtn.Click += new System.EventHandler(this.ButtonClicked);
+            this.DButton.Interval = 1;
+            this.DButton.Tick += new System.EventHandler(this.D_Tick);
             // 
-            // panel1
+            // AButton
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.panel1.Controls.Add(this.TxtValue);
-            this.panel1.Controls.Add(this.SetBtn);
-            this.panel1.Controls.Add(this.DecrementBtn);
-            this.panel1.Controls.Add(this.IncrementBtn);
-            this.panel1.Location = new System.Drawing.Point(630, 310);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(235, 176);
-            this.panel1.TabIndex = 4;
-            // 
-            // TxtValue
-            // 
-            this.TxtValue.Location = new System.Drawing.Point(104, 28);
-            this.TxtValue.Name = "TxtValue";
-            this.TxtValue.Size = new System.Drawing.Size(100, 20);
-            this.TxtValue.TabIndex = 4;
-            this.TxtValue.Text = "Enter value to set";
-            this.TxtValue.Click += new System.EventHandler(this.TxtValue_Clicked);
+            this.AButton.Interval = 1;
+            this.AButton.Tick += new System.EventHandler(this.A_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(864, 485);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.DisplayLbl);
+            this.Controls.Add(this.lblPlayer);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,12 +85,11 @@ namespace SoFarNoName
 
         #endregion
 
-        private Label DisplayLbl;
-        private Button DecrementBtn;
-        private Button IncrementBtn;
-        private Button SetBtn;
-        private Panel panel1;
-        private TextBox TxtValue;
+        private Label lblPlayer;
+        private Timer WButton;
+        private Timer SButton;
+        private Timer DButton;
+        private Timer AButton;
     }
 }
 
--- SoFarNoName/SoFarNoName/Form1.cs
+++ SoFarNoName/SoFarNoName/Form1.cs
@@ -13,38 +13,64 @@ namespace SoFarNoName
 {
     public partial class Form1 : Form
     {
+        public int speed = 3;
+
         public int count = 0;
         public Form1()
         {
             InitializeComponent();
             this.Text = "WinForm Course Session 1";
+            this.KeyPreview = true;
         }
 
-        private void ButtonClicked(object sender, EventArgs e)
-        {
-            Button btn = (Button)sender;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {            
 
-            if (btn.Name.ToLower().Contains("increment"))
-            {
-                count++;
-                DisplayLbl.Text = $"{count}";
-            }
-            else if (btn.Name.ToLower().Contains("decrement"))
-            {
-                count--;
-                DisplayLbl.Text = $"{count}";
-            }
-            else if (btn.Name.ToLower().Contains("set"))
-            {
-                int setValue;
-                bool Parsable = int.TryParse(TxtValue.Text, out setValue);
-                if (Parsable) { count = setValue; } else { TxtValue.Text = "Please enter a number"; }
-                DisplayLbl.Text = $"{count}";
-            }
+            if (e.KeyCode == Keys.W)
+                WButton.Start();
+
+            if (e.KeyCode == Keys.S)
+                SButton.Start();
+
+            if (e.KeyCode == Keys.A)
+                AButton.Start();
+
+            if (e.KeyCode == Keys.D)
+                DButton.Start();
         }
-        private void TxtValue_Clicked(object sender, EventArgs e)
+
+
+
+        private void W_Tick(object sender, EventArgs e)
         {
-            TxtValue.Text = "";
+            lblPlayer.Top -= speed;
+        }
+        private void S_Tick(object sender, EventArgs e)
+        {
+            lblPlayer.Top += speed;
+        }
+        private void A_Tick(object sender, EventArgs e)
+        {
+            lblPlayer.Left -= speed;
+        }
+        private void D_Tick(object sender, EventArgs e)
+        {
+            lblPlayer.Left += speed;
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+                WButton.Stop();
+            if (e.KeyCode == Keys.S)
+                SButton.Stop();
+            if (e.KeyCode == Keys.A)
+                AButton.Stop();
+            if (e.KeyCode == Keys.D)
+                DButton.Stop();
         }
     }
+
+      
 }
