0f3ae81f9f821e40bd2d6ebfd36336f025f2efaa
Pqoeodj <trevornggo@gmail.com>
2026-02-23 10:26:43 +0000

    ok started a real project with a goal of making a game. this time i cleaned up the code and made a moving label 'player' using only one timer

--- SoFarNoName/SoFarNoName/Form1.cs
+++ SoFarNoName/SoFarNoName/Form1.cs
@@ -14,70 +14,51 @@ namespace SoFarNoName
 {
     public partial class Form1 : Form
     {
-        const string SaveFile = "SavedTask.txt";
+        int playerSpeed = 10;
 
         public Form1()
         {
             InitializeComponent();
-            this.Text = "WinForm Course Session 7";
+            this.Text = "little surviver";
             this.KeyPreview = true;
-            ColorComboBox.Items.Add("Red");
-            ColorComboBox.Items.Add("Green");
-            ColorComboBox.Items.Add("Blue");
-            if (File.Exists(SaveFile))
-            {
-                foreach(string line in File.ReadAllLines(SaveFile))
-                {
-                    TaskListBox.Items.Add(line);
-                }
+            CheckState.Start();
+        }
 
-            }
+        private void PlayerLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+      
+        private bool up, down, left, right;
+        private void Game_Interval(object sender, EventArgs e)
+        {
+            int VerticalMovement = 0;
+            int HorizontalMovement = 0;
+            
+            if (up) VerticalMovement--;
+            if (down) VerticalMovement++;
+            if (left) HorizontalMovement--;
+            if (right) HorizontalMovement++;
+
+            PlayerLbl.Top += playerSpeed * VerticalMovement;
+            PlayerLbl.Left += playerSpeed * HorizontalMovement;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W) up = true;
+            if (e.KeyCode == Keys.S) down = true;
+            if (e.KeyCode == Keys.A) left = true;
+            if (e.KeyCode == Keys.D) right = true;
 
         }
 
-        private void AddBtn_Click(object sender, EventArgs e)
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            if(TxtTask.Text == "Name of Task")
-            {
-                AddBtn.Text = "Please enter a task in the text box above";
-            }
-            else
-            {
-                TaskListBox.Items.Add(TxtTask.Text);
-                
-            }
-        }
-
-        private void TxtTask_Click(object sender, EventArgs e)
-        {
-            TxtTask.Text = "";
-        }
-
-        private void RemoveBtn_Click(object sender, EventArgs e)
-        {
-            if(TaskListBox.SelectedIndex != -1)
-            {
-                TaskListBox.Items.RemoveAt(TaskListBox.SelectedIndex);
-            }
-        }
-
-        private void ColorComboBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            string SelectedColor = ColorComboBox.SelectedItem.ToString();
-            if (SelectedColor == "Red") this.BackColor = Color.Red;
-            if (SelectedColor == "Blue") this.BackColor = Color.Blue;
-            if (SelectedColor == "Green") this.BackColor = Color.Green;
-        }
-
-        private void SaveBtn_Click(object sender, EventArgs e)
-        {
-            List<string> currentTasks = new List<string>();
-
-            foreach(string items in TaskListBox.Items)
-            {
-                currentTasks.Add(items);
-            }
-            File.WriteAllLines(SaveFile, currentTasks.ToArray());
+            if (e.KeyCode == Keys.W) up = false;
+            if (e.KeyCode == Keys.S) down = false;
+            if (e.KeyCode == Keys.A) left = false;
+            if (e.KeyCode == Keys.D) right = false;
         }
     }
 
